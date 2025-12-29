78609c5c68ba3990f181fc765338cf5990b96b39
CHINNAKRISHNA143 <chinnikrishnamekala1@gmail.com>
2025-12-29 20:14:46 +0530

    Updated code for createAccount,Deposite and Withdraw functions

--- script.js
+++ script.js
@@ -14,6 +14,58 @@ function createAccount(){
         balance : documnet.getElementById("c-balance").value
     };
 
+    fetch(BASE_URL+"/accounts/create" ,{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(result => {
+        document.getElementById("create-result").innerText = "Account is Created and here is your Account Number "+result.accountNumber;
+    });
     
 
 }
+
+function deposite(){
+    const data = {
+        accNo : document.getElementById("d-acc").value,
+        amount : document.getElementById("d-amount").value
+    };
+
+    fetch(BASE_URL+"/transactions/deposite" ,{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(data)
+    })
+    .then(res => res.text())
+    .then(msg => {
+        document.getElementById("deposite-result").innerText = msg;
+    });
+
+}
+
+function withdraw(){
+    const data = {
+        accNo : document.getElementById("w-acc").value,
+        amount : document.getElementById("w-amount").value
+    };
+
+    fetch(BASE_URL+"/transactions/withdraw" ,{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(data)
+    })
+    .then(res => res.text())
+    .then(msg => {
+        document.getElementById("withdraw-result").innerText = msg;
+    });
+
+}
+
